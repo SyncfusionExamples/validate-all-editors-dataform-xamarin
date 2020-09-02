8aa58a8304747a12ee942e2f9927db26235b43b1
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-09-02 16:00:32 +0530

    XAMARIN-35819 - Sample added.

---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/Model/DataFormModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataFormXamarin
+{
+    public class DataFormModel
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string newPassword;
+        private string confirmPassword;
+        private string comments;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Value should not be empty")]
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Value should not be empty")]
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Value should not be empty")]
+        public string NewPassword
+        {
+            get { return newPassword; }
+            set { newPassword = value; }
+        }
+
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Value should not be empty")]
+        public string ConfirmPassword
+        {
+            get { return confirmPassword; }
+            set { confirmPassword = value; }
+        }
+    }
+}
---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/ViewModel/DataFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataFormXamarin
+{
+    public class DataFormViewModel
+    {
+        private DataFormModel loginInfo;
+        public DataFormModel LoginInfo
+        {
+            get { return loginInfo; }
+            set { loginInfo = value; }
+        }
+
+        public DataFormViewModel()
+        {
+            loginInfo = new DataFormModel();
+            this.ValidateCommand = new Command<object>(this.OnValidate);
+        }
+        public Command<object> ValidateCommand { get; set; }
+        private void OnValidate(object dataForm)
+        {
+            var dataFormLayout = dataForm as Syncfusion.XForms.DataForm.SfDataForm;
+            dataFormLayout.Validate();
+        }
+    }
+}
